8e522179f57aa06aa1530ef2e10ff5356e4fb13c
Khaled-Ballour <ballourk@gmail.com>
2023-05-01 19:08:44 +0300

    fetch first country

--- script.js
+++ script.js
@@ -4,31 +4,38 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
-request.send(); //Async
+const getCountry = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send(); //Async
 
-request.addEventListener('load', function () {
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
-  const html = `
-  <article class="country">
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    const html = `
+    <article class="country">
     <img class="country__img" src="${data.flags.png}" />
     <div class="country__data">
-      <h3 class="country__name">${data.name.common}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
-      ).toFixed(1)} people</p>
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} people</p>
       <p class="country__row"><span>🗣️</span>${
         Object.values(data.languages)[0]
       }</p>
       <p class="country__row"><span>💰</span>${
         Object.values(data.currencies)[0].name
       }</p>
-    </div>
-  </article>
-`;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-});
+      </div>
+      </article>
+      `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountry('germany');
+getCountry('usa');
+getCountry('russia');
+getCountry('ukraine');
